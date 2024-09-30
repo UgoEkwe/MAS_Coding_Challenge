42798d275377960e1ef6ff60783cfb19928d78fb
Ugonna Chaekwe <39320450+UgoEkwe@users.noreply.github.com>
2024-09-30 12:13:10 -0500

    Initial Commit

---
+++ MAS Coding Challenge/MAS_Coding_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAS_Coding_ChallengeApp.swift
+//  MAS Coding Challenge
+//
+//  Created by Ugonna Oparaochaekwe on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MAS_Coding_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
